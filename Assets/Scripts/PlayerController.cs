8468c5289978d5fbe886582e55a6f735c7b22a59
Ruben Rådelius <ruben.radelius@gmail.com>
2021-07-06 13:33:12 +0200

    SUM-21 Add collision check and basic ground check

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,29 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     [SerializeField] float speed = 12f;
     [SerializeField] float gravity = -10f;
     Vector3 _move = Vector3.zero;
+    bool _isGrounded;
 
     void Update() {
 
         _move.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-        _move.y = gravity * Time.deltaTime;
         
+        if (!_isGrounded)
+            _move.y = gravity * Time.deltaTime;
+        else 
+            _move.y = 0;
+
         transform.position += _move;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        _isGrounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        _isGrounded = false;
     }
 }
