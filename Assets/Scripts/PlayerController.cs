6160ddd6aa80e5728801424a912c115d520726fc
Ruben Rådelius <ruben.radelius@gmail.com>
2021-07-06 14:53:32 +0200

    SUM-21 SUM-6 Add ground check using bottom corners of player collider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,18 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
+
 public class PlayerController : MonoBehaviour {
     [SerializeField] float speed = 12f;
     [SerializeField] float gravity = -10f;
     Vector3 _move = Vector3.zero;
     bool _isGrounded;
+    Collider2D _col;
+
+    void Awake() {
+        _col = GetComponent<Collider2D>();
+    }
 
     void Update() {
 
@@ -20,7 +27,17 @@ public class PlayerController : MonoBehaviour {
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        _isGrounded = true;
+        Vector3 extents = _col.bounds.extents;
+        Vector3 position = transform.position;
+        
+        GroundCheck(other, position, extents);
+    }
+
+    void GroundCheck(Collider2D other, Vector3 position, Vector3 extents) {
+        position.y -= extents.y + 0.05f;
+        var bottomLeftCorner = new Vector3(position.x - extents.x, position.y, 0);
+        var bottomRightCorner = new Vector3(position.x + extents.x, position.y, 0);
+        _isGrounded = other.OverlapPoint(bottomLeftCorner) || other.OverlapPoint(bottomRightCorner);
     }
 
     void OnTriggerExit2D(Collider2D other) {
