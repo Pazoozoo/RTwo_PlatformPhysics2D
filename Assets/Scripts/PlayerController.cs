4bfaf338682175104e7147266e0786f32107ad94
Ruben Rådelius <ruben.radelius@gmail.com>
2021-07-06 13:16:30 +0200

    SUM-6 Add gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     [SerializeField] float speed = 12f;
-    [SerializeField] float gravity;
-    Vector3 _move;
+    [SerializeField] float gravity = -10f;
+    Vector3 _move = Vector3.zero;
 
     void Update() {
 
-        _move.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime; 
+        _move.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        _move.y = gravity * Time.deltaTime;
         
         transform.position += _move;
     }
