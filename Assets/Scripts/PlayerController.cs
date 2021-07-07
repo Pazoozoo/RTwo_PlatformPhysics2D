ef6de30758bf5ddbb56dd6dc93a953daaa75ded2
Ruben Rådelius <ruben.radelius@gmail.com>
2021-07-07 15:56:24 +0200

    SUM-7 Add basic jump logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     [SerializeField] float speed = 12f;
     [SerializeField] float gravity = -10f;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] float jumpDecay = 1f;
     Vector3 _move = Vector3.zero;
+    float _jumpVelocity;
     bool _isGrounded;
+    bool _hasJumped;
     Collider2D _col;
 
     void Awake() {
@@ -15,9 +19,18 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Update() {
-
         _move.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
         _move.y = _isGrounded ? 0f : gravity * Time.deltaTime;
+        
+        if (Input.GetButton("Jump") && !_hasJumped) {
+            _jumpVelocity = jumpForce;
+            _hasJumped = true;
+        }
+        
+        if (_hasJumped) {
+            _jumpVelocity = _jumpVelocity > 0f ? _jumpVelocity - jumpDecay * Time.deltaTime : 0f;
+            _move.y += _jumpVelocity * Time.deltaTime;
+        }
 
         transform.position += _move;
     }
@@ -29,11 +42,13 @@ public class PlayerController : MonoBehaviour {
         GroundCheck(other, position, extents);
     }
 
-    void GroundCheck(Collider2D other, Vector3 position, Vector3 extents) {
-        position.y -= extents.y + 0.05f;
+    void GroundCheck(Collider2D other, Vector3 position, Vector3 extents, float offset = 0.05f) {
+        position.y -= extents.y + offset;
         var bottomLeftCorner = new Vector3(position.x - extents.x, position.y, 0);
         var bottomRightCorner = new Vector3(position.x + extents.x, position.y, 0);
         _isGrounded = other.OverlapPoint(bottomLeftCorner) || other.OverlapPoint(bottomRightCorner);
+        if (_isGrounded)
+            _hasJumped = false;
     }
 
     void OnTriggerExit2D(Collider2D other) {
