4887bafc9204ca04f8edc13c03cbc6f5a9b7f050
Ruben Rådelius <ruben.radelius@gmail.com>
2021-07-07 20:57:19 +0200

    SUM-7 Add increased fall speed when coming down after a jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,18 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Collider2D))]
 
 public class PlayerController : MonoBehaviour {
+    public Text jumpText;
     [SerializeField] float speed = 12f;
     [SerializeField] float gravity = -8f;
     [SerializeField] float jumpForce = 25f;
     [SerializeField] float jumpDecay = 50f;
+    [SerializeField] float maxFallSpeed = -10f;
+    [SerializeField] float fallSpeedMultiplier = 1.1f;
+    [SerializeField] float fallSpeedThreshold = 0f;
     Vector3 _move = Vector3.zero;
     Vector3 _playerExtents;
     float _jumpVelocity;
@@ -30,17 +35,28 @@ public class PlayerController : MonoBehaviour {
         }
         
         if (_hasJumped) {
-            _jumpVelocity = _jumpVelocity > 0f ? _jumpVelocity - jumpDecay * Time.deltaTime : 0f;
+            _jumpVelocity -= jumpDecay * Time.deltaTime;
+
+            if (_jumpVelocity < fallSpeedThreshold)
+                _jumpVelocity *= _jumpVelocity > 0 ? -fallSpeedMultiplier : fallSpeedMultiplier;
+
+            if (_jumpVelocity <= maxFallSpeed)
+                _jumpVelocity = maxFallSpeed;
+            
             _move.y += _jumpVelocity * Time.deltaTime;
         }
 
         transform.position += _move;
     }
 
+    void FixedUpdate() {
+        jumpText.text = _jumpVelocity.ToString("F");
+    }
+
     void OnTriggerEnter2D(Collider2D other) {
         Vector3 playerPosition = transform.position;
         _isGrounded = GroundCheck(other, playerPosition);
-        
+        Debug.Log($"Enter: grounded = {_isGrounded}");
         if (_isGrounded) {
             Bounds ground = other.bounds;
             float groundHeight = ground.center.y + ground.extents.y;
@@ -52,7 +68,7 @@ public class PlayerController : MonoBehaviour {
     }
 
     bool GroundCheck(Collider2D other, Vector3 position) {
-        position.y -= _playerExtents.y;
+        position.y -= _playerExtents.y + 0.05f;
         var bottomLeftCorner = new Vector3(position.x - _playerExtents.x, position.y, 0);
         var bottomRightCorner = new Vector3(position.x + _playerExtents.x, position.y, 0);
         return other.OverlapPoint(bottomLeftCorner) || other.OverlapPoint(bottomRightCorner);
@@ -60,5 +76,6 @@ public class PlayerController : MonoBehaviour {
 
     void OnTriggerExit2D(Collider2D other) {
         _isGrounded = false;
+        Debug.Log($"Exit: grounded = {_isGrounded}");
     }
 }
